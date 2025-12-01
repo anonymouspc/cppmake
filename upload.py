e8dfee2329bbac9633741aa5e6a110df0517d203
anonymouspc <shyeyian@petalmail.com>
2025-12-01 15:05:57 +0800

    update

--- upload.py
+++ upload.py
@@ -5,10 +5,11 @@ import subprocess
 for project in ["cppmakelib", "cppmake"]:
     with open(f"{project}/pyproject.toml", 'r') as reader:
         pyproject = reader.read()
-    pyproject = re.sub(r'^version = "(\d+)"$', lambda match: f'version = "{int(match.group(1)) + 1}"', pyproject, flags=re.MULTILINE)
+    pyproject = re.sub(r'^version = "(\d+)\.(\d+)\.(\d+)"$', lambda match: f'version = "{match.group(1)}.{match.group(2)}.{int(match.group(3)) + 1}"', pyproject, flags=re.MULTILINE)
     with open(f"{project}/pyproject.toml", 'w') as writer:
         writer.write(pyproject)
     subprocess.run(f"python -m build",                                                                     shell=True, cwd=project, check=True)
     subprocess.run(f"twine upload dist/* --username __token__ --password {open("pypi_token.txt").read()}", shell=True, cwd=project, check=True)
-    shutil.rmtree(f"{project}/dist",               ignore_errors=True)
-    shutil.rmtree(f"{project}/{project}.egg-info", ignore_errors=True)
+    subprocess.run(f"pip install --upgrade {project}",                                                     shell=True,              check=True)
+    shutil.rmtree(f"{project}/dist")
+    shutil.rmtree(f"{project}/{project}.egg-info")
