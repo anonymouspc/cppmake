fd215045e34f31fda5f0f2583dbacafafaa23393
anonymous <shyeyian@petalmail.com>
2025-12-22 10:10:29 +0800

    update

--- upload.py
+++ upload.py
@@ -2,14 +2,15 @@ import re
 import shutil
 import subprocess
 
+subprocess.run(f"git pull", shell=True, check=False)
 for project in ["cppmakelib", "cppmake"]:
     with open(f"{project}/pyproject.toml", 'r') as reader:
         pyproject = reader.read()
     pyproject = re.sub(r'^version = "(\d+)\.(\d+)\.(\d+)"$', lambda match: f'version = "{match.group(1)}.{match.group(2)}.{int(match.group(3)) + 1}"', pyproject, flags=re.MULTILINE)
     with open(f"{project}/pyproject.toml", 'w') as writer:
         writer.write(pyproject)
-    subprocess.run(f"python -m build",                                                                                          shell=True, cwd=project, check=True)
-    subprocess.run(f"twine upload dist/* --username __token__ --password {open("../token/pypi.personal-access.token").read()}", shell=True, cwd=project, check=True)
+    subprocess.run(f"python -m build",                                                                     shell=True, cwd=project, check=True)
+    subprocess.run(f"twine upload dist/* --username __token__ --password {open("pypi-token.txt").read()}", shell=True, cwd=project, check=True)
     shutil.rmtree(f"{project}/dist")
     shutil.rmtree(f"{project}/{project}.egg-info")
 subprocess.run(f"git add .",            shell=True, check=False)
