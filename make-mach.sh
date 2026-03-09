ef4136ff43e577274b48b7394c180ddc30c06ed7
shyeyian <shyeyian@icloud.com>
2026-03-10 01:00:39 +0800

    add boost as submodule

--- make-mach.sh
+++ make-mach.sh
@@ -1,2 +1,8 @@
-mkdir -p .cppmake
-clang++ -std=c++26 -Ipkg/boost/asio/include bin/cppmake.cpp -o .cppmake/bin/cppmake
+# Make packages
+mkdir -p .cppmake/pkg
+pkg/boost/bootstrap.sh
+pkg/boost/b2 --with-asio --with-process link=static --build-dir=.cppmake/pkg/boost/build --prefix=.cppmake/pkg/boost/install 
+
+# Make cppmake
+mkdir -p .cppmake/bin
+clang++ -std=c++26 -I.cppmake/pkg/boost/install/include bin/cppmake.cpp -o .cppmake/bin/cppmake
